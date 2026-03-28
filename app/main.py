06aaf2a18591dc55cfa9bc2f2af76fc92fcd2007
Deepanshu-24 <deepanshusaini240611@gmail.com>
2026-03-28 16:04:28 +0530

    added middleware and cors headers

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .models import Base
 from .database import engine
 from .routers import auth,admin,users,posts,likes,comments
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
@@ -20,3 +21,17 @@ app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(likes.router)
 app.include_router(comments.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+      # add if running on port 3000
+        "http://127.0.0.1:5173",
+        # "https://your-deployed-url.com",  # add when deployed
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
