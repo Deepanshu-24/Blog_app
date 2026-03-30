86946fddc2ff19a3880d41f05752a866805a7ab4
Deepanshu-24 <deepanshusaini240611@gmail.com>
2026-03-30 12:20:57 +0530

    Using PostgreSQL database

--- app/database.py
+++ app/database.py
@@ -5,9 +5,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
+SQL_ALCHEMY_DATABASE_URL = os.environ.get("DATABASE_PATH")
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False , autoflush=False, bind=engine)
 
